fa888aaa1fd6059714b1ede05f51eac28d56d8d0
David Eini <DavidE@call.local>
2018-05-10 12:43:11 +0300

    sync

--- pages/run/run-menu.js
+++ pages/run/run-menu.js
@@ -70,7 +70,7 @@ const findReplace = projectSettings => [{
     to: `./${projectSettings.path}/app.module`
   },
   {
-    from: /\[IIS Host\]/i,
+    from: /\[IIS Host\]/g,
     to: `${projectSettings.qflowIISHost}`
   }
 ];
