73228682f115f15f0a7a99635e58e35b09f979eb
David Eini <DavidE@call.local>
2018-05-10 12:58:33 +0300

    sync

--- pages/run/run-menu.js
+++ pages/run/run-menu.js
@@ -100,6 +100,13 @@ const configSubProject$ = projectSettings =>
   )
   .switchMap(() => replaceInFile$(joinCWD('src', `${projectSettings.entry}.ts`), findReplace(projectSettings)))
   .switchMap(() => replaceInFile$(joinCWD('src', `README.${projectSettings.entry}.md`), findReplace(projectSettings)));
+.switchMap(() => replaceInFile$(joinCWD('src', `README.${projectSettings.entry}.md`), [{
+  from: projectSettings.qflowIISHost,
+  to: '../'
+}, {
+  from: /\/+/,
+  to: '/'
+}]));
 
 const configIfNotExists = projectSettings =>
   existsAll$([
