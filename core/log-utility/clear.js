dbb6028448a09eba115edaa1889f603aae5e0cb7
David Eini <DavidE@call.local>
2018-05-14 01:42:15 +0300

    sync

--- core/log-utility/clear.js
+++ core/log-utility/clear.js
@@ -1,4 +1,4 @@
 // const clear = () => process.stdout.write('\033c') || console.log('\033c');
-const clear = () => console.log('\033c');
+const clear = () => process.stdout.write('\x1B[2J\x1B[0f');
 
 module.exports = clear;
